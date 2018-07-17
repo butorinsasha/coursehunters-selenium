033ebf98d90b4ecd7547939065e63f3035b6ca19
butorin <butorin@spb.rusoft-company.ru>
2018-07-17 19:00:07 +0300

    lesson011firstprogram is comleted

---
+++ src/test/java/lesson011firstprogram/TestClass.java
@@ -0,0 +1,9 @@
+package lesson011firstprogram;
+
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.print("It's my first program!");
+        System.out.println("1234");
+    }
+}
