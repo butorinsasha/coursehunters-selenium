3baa63934999a40d25c9623bf98bfdc3bba61368
butorin <butorin@spb.rusoft-company.ru>
2018-07-25 13:18:27 +0300

    lesson022method is done

---
+++ src/test/java/lesson022methods/MethodsTest.java
@@ -0,0 +1,52 @@
+package lesson022methods;
+
+public class MethodsTest {
+    public static void main(String[] args) {
+        int a = 13;
+        int b = 3;
+        int sum;
+        sum = getSum(a, b);
+        System.out.println(sum);
+        System.out.println(getSum(2, 11));
+
+        showSum(10, 51, 77);
+        showSum(5, 12, 14);
+
+        saySmth();
+        saySmth();
+        saySmth();
+
+        sayHello("bro");
+        String gst = "guest";
+        sayHello(gst);
+        showSumToPerson("sasha", 15, 11, 33);
+    }
+
+    static int getSum(int x, int y) {
+        int sum;
+        sum = x + y;
+        return sum;
+    }
+
+    static void showSum(int x, int y, int z) {
+        int sum = x + y +z;
+        System.out.println("Sum is: " + sum);
+    }
+
+    static void saySmth() {
+        System.out.println("Something is said");
+    }
+
+    static void  sayHello(String name) {
+        System.out.println("Hello, " + name);
+        System.out.println("Got some money?");
+    }
+
+    static void showSumToPerson(String name, int a, int b, int c) {
+        System.out.println("*** START showSumToPerson() method***");
+        sayHello(name);
+        showSum(a, b, c);
+        System.out.println("*** END showSumToPerson() method***");
+    }
+
+}
