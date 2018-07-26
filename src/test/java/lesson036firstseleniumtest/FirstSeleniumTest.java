3d6a43a24e7b1c90380641b0503908fa18c6cf8f
butorin <butorin@spb.rusoft-company.ru>
2018-07-26 18:16:25 +0300

    lesson036firstseleniumtest in progress: only one @Test annotated method is run properly, second one leads to SessionNotCreatedException: Unable to find a matching set of capabilities

---
+++ src/test/java/lesson036firstseleniumtest/FirstSeleniumTest.java
@@ -0,0 +1,35 @@
+package lesson036firstseleniumtest;
+
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstSeleniumTest {
+
+    WebDriver driver = new ChromeDriver();
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\butorin\\Documents\\work\\qa\\coursehunters-selenium\\drivers\\geckodriver.exe");
+        driver.get("https://www.seleniumhq.org/");
+    }
+
+    @Test
+    public void downloadsPageHasRightTitle(){
+        driver.findElement(By.id("menu_download")).click();
+        Assert.assertEquals("Downloads", driver.getTitle());
+    }
+
+    /*intended to fail*/
+    @Test
+    public void projectsPageHasRightTitle(){
+        driver.findElement(By.id("menu_projects")).click();
+        Assert.assertEquals("Projects", driver.getTitle()); // it is actually "Selenium Projects"
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
