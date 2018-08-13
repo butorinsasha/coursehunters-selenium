add5a4252e5cb6a60e708ab95f16fe8d52f34d14
butorin <butorin@spb.rusoft-company.ru>
2018-08-13 17:06:31 +0300

    lesson043buttons is done

---
+++ src/test/java/lesson043buttons/ButtonsTest.java
@@ -0,0 +1,51 @@
+package lesson043buttons;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ButtonsTest {
+
+    WebDriver driver;
+    WebDriver driver2;
+
+    @Before
+    public void setUp() {
+//        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\drivers\\chromedriver.exe");
+
+        driver = new ChromeDriver();
+//        driver.manage().window().setSize(new Dimension(900,500));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        driver.manage().window().setPosition(new Point(0,0));
+
+    }
+
+    @Test
+    public void pushTheButtons() {
+        driver.get("https://en.wikipedia.org");
+        driver.findElement(By.xpath("//input[@id=\"searchButton\"]")).click();
+
+        driver.navigate().to("http://github.com");
+        WebElement button = driver.findElement(By.xpath("//form[@class=\"home-hero-signup js-signup-form\"]//button"));
+        System.out.println("Button text is: " + button.getText());
+        Assert.assertEquals("Sign up for GitHub", button.getText());
+//        button.submit();
+        driver.findElement(By.xpath("//a[text()=\"Sign in\"]")).click();
+
+
+        driver.navigate().to("https://www.facebook.com/");
+        driver.findElement(By.xpath("//label[@id=\"loginbutton\"]//input")).submit();
+    }
+
+    @After
+    public void tearTown() {
+        driver.quit();
+    }
+
+}
