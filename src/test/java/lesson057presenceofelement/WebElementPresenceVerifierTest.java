260337f5198f89835053ba08b5b9470cd22b8a7f
butorin <butorin@spb.rusoft-company.ru>
2018-09-21 19:30:13 +0300

    lesson057presenceofelement is in progress

---
+++ src/test/java/lesson057presenceofelement/WebElementPresenceVerifierTest.java
@@ -0,0 +1,42 @@
+package lesson057presenceofelement;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WebElementPresenceVerifierTest {
+    WebDriver driver;
+    WebDriverWait wait;
+    JavascriptExecutor jse;
+    Actions actions;
+
+    @Before
+    public void setUp() throws Exception {
+        
+    }
+
+    @Test
+    public void testPresenceOfElementByFindElementsSizeListIsGraterThanZero() {
+        
+    }
+
+    @Test
+    public void testPresenceOfElementByNoSuchElementExceptionHandling() {
+
+    }
+
+    @Test
+    public void testPresenceOfElementByPresenceOfElementLocated() {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        
+    }
+}
