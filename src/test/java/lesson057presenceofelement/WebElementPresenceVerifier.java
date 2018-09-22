4d2744d4c048ed6ede72e74c99724912d34c6abf
butorin <butorin@spb.rusoft-company.ru>
2018-09-22 12:39:48 +0300

    lesson057presenceofelement testPresenceOfElementByFindElementsSizeListIsGreaterThanZero version 1.0

---
+++ src/test/java/lesson057presenceofelement/WebElementPresenceVerifier.java
@@ -0,0 +1,5 @@
+package lesson057presenceofelement;
+
+public class WebElementPresenceVerifier {
+
+}
--- src/test/java/lesson057presenceofelement/WebElementPresenceVerifierTest.java
+++ src/test/java/lesson057presenceofelement/WebElementPresenceVerifierTest.java
@@ -1,11 +1,14 @@
 package lesson057presenceofelement;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -17,16 +20,39 @@ public class WebElementPresenceVerifierTest {
 
     @Before
     public void setUp() throws Exception {
-        
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 5);
+        jse = (JavascriptExecutor) driver;
+        actions = new Actions(driver);
     }
 
     @Test
-    public void testPresenceOfElementByFindElementsSizeListIsGraterThanZero() {
-        
+    public void testPresenceOfElementByFindElementsSizeListIsGreaterThanZero() {
+        driver.navigate().to("https://github.com/");
+
+        String aSigninXpath = "//a[text()=\"Sign in\"]";
+        String aLoginXpath = "//a[text()=\"Login in\"]";
+
+        boolean isPresent;
+
+        isPresent = driver.findElements(By.xpath(aSigninXpath)).size() > 0;
+        System.out.println(isPresent);
+        Assert.assertTrue(isPresent);
+
+        isPresent = driver.findElements(By.xpath(aLoginXpath)).size() > 0;
+        System.out.println(isPresent);
+        Assert.assertFalse(isPresent);
+
     }
+
 
     @Test
     public void testPresenceOfElementByNoSuchElementExceptionHandling() {
+        driver.get("https://github.com/");
+
+        String aSigninXpath = "//a[text()=\"Sign in\"]";
+        String aLoginXpath = "//a[text()=\"Login in\"]";
+
 
     }
 
@@ -37,6 +63,6 @@ public class WebElementPresenceVerifierTest {
 
     @After
     public void tearDown() throws Exception {
-        
+        driver.quit();
     }
 }
