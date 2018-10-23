73ed3870c9cb164789805816a7ba44b60b313f53
butorin <butorin@spb.rusoft-company.ru>
2018-10-23 18:47:12 +0300

    MainPage.java created

---
+++ src/main/java/lesson063PageObject/LoginPage.java
@@ -0,0 +1,11 @@
+package lesson063PageObject;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
---
+++ src/main/java/lesson063PageObject/SignUpPage.java
@@ -0,0 +1,11 @@
+package lesson063PageObject;
+
+import org.openqa.selenium.WebDriver;
+
+public class SignUpPage {
+    WebDriver driver;
+
+    public SignUpPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
