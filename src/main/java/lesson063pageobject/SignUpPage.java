69d3022258b634a23a97192a12435c282d1a70bb
butorin <butorin@spb.rusoft-company.ru>
2018-10-24 16:03:39 +0300

    MainPageTest done

--- src/main/java/lesson063pageobject/SignUpPage.java
+++ src/main/java/lesson063pageobject/SignUpPage.java
@@ -20,12 +20,58 @@ public class SignUpPage extends Page {
     private By signUpButton = By.xpath("//button[@id=\"signup_button\"]");
 
     private By mainError = By.xpath("//form[@id=\"signup-form\"]//div[@class=\"flash flash-error my-3\"]");
-    private By loginError = By.xpath("//form[@id=\"signup-form\"]//auto-check[@src=\"/signup_check/username\"]//dd[@class=\"error\"]");
+    private By usernameError = By.xpath("//form[@id=\"signup-form\"]//auto-check[@src=\"/signup_check/username\"]//dd[@class=\"error\"]");
     private By emailError = By.xpath("//form[@id=\"signup-form\"]//auto-check[@src=\"/signup_check/email\"]//dd[@class=\"error\"]");
     private By passwordError = By.xpath("//form[@id=\"signup-form\"]//password-strength//dd[@class=\"error\"]");
 
-    private By usernameNote = By.xpath("//form[@id=\"signup-form\"]//auto-check[@src=\"/signup_check/username\"]//dd[@class=\"error\"]");
-    private By emailNote = By.xpath("//form[@id=\"signup-form\"]//auto-check[@src=\"/signup_check/email\"]//dd[@class=\"error\"]");
+    private By usernameNote = By.xpath("//form[@id=\"signup-form\"]//auto-check[@src=\"/signup_check/usernameemail\"]//p[@class=\"note\"]");
+    private By emailNote = By.xpath("//form[@id=\"signup-form\"]//auto-check[@src=\"/signup_check/email\"]//p[@class=\"note\"]");
+
+    private By passwordNote = By.xpath("//form[@id=\"signup-form\"]//password-strength//p[@class=\"note\"]");
+
+    public By getHeader() {
+        return header;
+    }
+
+    public By getUsernameField() {
+        return usernameField;
+    }
+
+    public By getEmailField() {
+        return emailField;
+    }
+
+    public By getPasswordField() {
+        return passwordField;
+    }
+
+    public By getSignUpButton() {
+        return signUpButton;
+    }
+
+    public By getMainError() {
+        return mainError;
+    }
+
+    public By getUsernameError() {
+        return usernameError;
+    }
+
+    public By getEmailError() {
+        return emailError;
+    }
+
+    public By getPasswordError() {
+        return passwordError;
+    }
+
+    public By getUsernameNote() {
+        return usernameNote;
+    }
+
+    public By getEmailNote() {
+        return emailNote;
+    }
 
     public SignUpPage typeUsername(String username) {
         driver.findElement(usernameField).sendKeys(username);
@@ -42,6 +88,10 @@ public class SignUpPage extends Page {
         return this;
     }
 
+    public By getPasswordNote() {
+        return passwordNote;
+    }
+
     public Page register(String username, String email, String password) {
         typeUsername(username);
         typeEmail(email);
@@ -54,15 +104,32 @@ public class SignUpPage extends Page {
         return driver.findElement(header).getText();
     }
 
-    public String getMainError() {
-        return driver.findElement(header).getText();
+    public String getMainErrorText() {
+        return driver.findElement(mainError).getText();
+    }
+
+    public String getUsernameErrorText() {
+        return driver.findElement(usernameError).getText();
     }
 
     public String getUsernameNoteText() {
         return driver.findElement(usernameNote).getText();
     }
 
-    public String getMainErrorText() {
-        return driver.findElement(mainError).getText();
+    public String getEmailErrorText() {
+        return driver.findElement(emailError).getText();
     }
+
+    public String getEmailNoteText() {
+        return driver.findElement(emailNote).getText();
+    }
+
+    public String getPasswordErrorText() {
+        return driver.findElement(passwordError).getText();
+    }
+
+    public String getPasswordNoteText() {
+        return driver.findElement(passwordNote).getText();
+    }
+
 }
--- src/test/java/lesson063pageobject/MainPageTest.java
+++ src/test/java/lesson063pageobject/MainPageTest.java
@@ -1,5 +1,6 @@
 package lesson063pageobject;
 
+import lesson057presenceofelement.WebElementPresenceVerifier;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -17,16 +18,44 @@ public class MainPageTest {
     }
 
     @Test
-    public void registerAtMainPageWithRightCreds() {
+    public void registerWithUnusedCreds() {
         driver.get("https://github.com/");
         MainPage mainPage = new MainPage(driver);
-        SignUpPage signUpPage = mainPage.register("butorinsasha5", "butorinsasha5@rupayamail.com", "M4Jokshr75Jjj");
+        SignUpPage signUpPage = mainPage.register("butorinsasha6", "butorinsasha6@rupayamail.com", "JOer33okokp");
         Assert.assertEquals("Join GitHub · GitHub", signUpPage.getPageTitle());
         Assert.assertEquals("https://github.com/join", signUpPage.getPageURL());
     }
 
+    @Test
+    public void registerWithUsedUsername() {
+        driver.get("https://github.com/");
+        MainPage mainPage = new MainPage(driver);
+        SignUpPage signUpPage = mainPage.register("butorinsasha", "butorinsasha@rupayamail.com", "JOer33okokp");
+
+        Assert.assertEquals("There were problems creating your account.", signUpPage.getMainErrorText());
+        Assert.assertTrue(signUpPage.getUsernameErrorText().contains("already taken"));
+
+        Assert.assertFalse(WebElementPresenceVerifier.isPresentByFindElementsSizeListIsGreaterThanZero(driver, signUpPage.getUsernameNote()));
+        Assert.assertFalse(WebElementPresenceVerifier.isPresentByFindElementsSizeListIsGreaterThanZero(driver, signUpPage.getPasswordError()));
+
+        Assert.assertEquals("We’ll occasionally send updates about your account to this inbox. We’ll never share your email address with anyone.", signUpPage.getEmailNoteText());
+        Assert.assertEquals("Make sure it's more than 15 characters, or at least 7 characters, including a number, and a lowercase letter.", signUpPage.getPasswordNoteText());
+    }
+
+    @Test
+    public void registrWithBlankFields() {
+        driver.get("https://github.com/");
+        MainPage mainPage = new MainPage(driver);
+        SignUpPage signUpPage = mainPage.register("", "", "");
+
+        Assert.assertEquals("There were problems creating your account.", signUpPage.getMainErrorText());
+        Assert.assertTrue(signUpPage.getUsernameErrorText().contains("can't be blank"));
+        Assert.assertTrue(signUpPage.getEmailErrorText().contains("can't be blank"));
+        Assert.assertTrue(signUpPage.getPasswordErrorText().contains("Password can't be blank and is too short (minimum is 7 characters)"));
+    }
+
     @After
     public void tearDown() throws Exception {
-        driver.quit();
+//        driver.quit();
     }
 }
